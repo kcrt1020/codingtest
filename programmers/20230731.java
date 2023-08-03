0ad52a9dfab5c7ba09ca340d774016ea4e432726
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-03 21:01:03 +0900

    20230803

--- programmers/20230731.java
+++ programmers/20230731.java
@@ -56,3 +56,19 @@ class Solution {
         return answer;
     }
 }
+
+
+// 수열과 구간 쿼리 3
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int temp;
+        
+        for(int i=0; i<queries.length; i++){         
+            temp = arr[queries[i][0]];
+            arr[queries[i][0]]=arr[queries[i][1]];
+            arr[queries[i][1]]=temp;
+        }
+        return arr;
+    }
+}
