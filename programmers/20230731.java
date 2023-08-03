919aac8b8e8d73f095d9e1fe5cd702bbb6af7cdb
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-03 22:10:00 +0900

    20230803

--- programmers/20230731.java
+++ programmers/20230731.java
@@ -72,3 +72,25 @@ class Solution {
         return arr;
     }
 }
+
+
+// 수열과 구간 쿼리 2
+
+class Solution {
+    private static final int INF = 1000001;
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        
+        for(int i=0; i<queries.length; i++){
+            int num=INF;
+            for(int j=queries[i][0]; j<=queries[i][1]; j++){
+                    if(arr[j]<=queries[i][2] || arr[j]>=num){
+                        continue;
+                    }
+                    num=arr[j];
+            }
+            answer[i] = num == INF ? -1 : num;
+        }
+        return answer;
+    }
+}
