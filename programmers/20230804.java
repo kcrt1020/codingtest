ad934c072236f843eda25745709137e7e93ce0d2
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-04 01:59:31 +0900

    20230804

--- programmers/20230804.java
+++ programmers/20230804.java
@@ -5,3 +5,48 @@ class Solution {
         return my_string.replace(letter,"");
     }
 }
+
+
+// 각도기
+
+class Solution {
+    public int solution(int angle) {
+        int answer = 0;
+        if(angle<90) 
+        {
+            answer = 1;
+        }
+        else if(angle==90) {
+            answer = 2;
+        }
+        else if(angle>90 && angle<180) {
+            answer = 3;
+        }
+        else if(angle==180) {
+            answer = 4;
+        }
+    return answer;
+    }
+}
+
+
+// 양꼬치
+
+class Solution {
+    public int solution(int n, int k) {
+        return (n*12000) + (k*2000) - (n/10*2000);
+    }
+}
+
+
+// 짝수의 합
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=0; i<=n; i+=2){
+            answer+=i;
+        }
+        return answer;
+    }
+}
