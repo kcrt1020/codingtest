01c94301b854189faa28314f0f234c4bd54d2688
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-12 23:01:10 +0900

    20230812

---
+++ programmers/20230812.java
@@ -0,0 +1,16 @@
+// 배열 자르기
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int position = num2-num1+1;
+        int[] answer = new int[position];
+        int j=0;
+        for(int i=0; i<numbers.length; i++){
+            if(i>=num1 && i<=num2) {
+                answer[j]=numbers[i];
+                j++;
+            }
+        }
+        return answer;
+    }
+}
