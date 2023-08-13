08e91aaf2fa506acf23b9ad69a150484528e16c8
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-13 23:34:41 +0900

    20230813

--- programmers/20230812.java
+++ programmers/20230812.java
@@ -1,16 +1,21 @@
+import java.util.*;
+
 // 배열 자르기
 
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int position = num2-num1+1;
-        int[] answer = new int[position];
-        int j=0;
-        for(int i=0; i<numbers.length; i++){
-            if(i>=num1 && i<=num2) {
-                answer[j]=numbers[i];
-                j++;
-            }
-        }
-        return answer;
+        // int position = num2-num1+1;
+        // int[] answer = new int[position];
+        // int j=0;
+        // for(int i=0; i<numbers.length; i++){
+        //     if(i>=num1 && i<=num2) {
+        //         answer[j]=numbers[i];
+        //         j++;
+        //     }
+        // }
+        // return answer;
+
+        return Arrays.copyOfRange(numbers, num1, num2 + 1);
     }
 }
+
