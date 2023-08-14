ff4b5933ddbf717ca6abad0f14c9f563c858492f
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-15 00:18:47 +0900

    20230815

--- programmers/20230812.java
+++ programmers/20230812.java
@@ -54,3 +54,18 @@ class Solution {
         return answer;
     }
 }
+
+
+// 순서쌍의 개수
+// 순서쌍의 개수는 약수의 개수와 같다
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
