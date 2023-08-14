3c02ffbea902f36cc2f3725468eca7c4fbb97f9c
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-14 20:41:15 +0900

    20230814

--- programmers/20230812.java
+++ programmers/20230812.java
@@ -19,3 +19,17 @@ class Solution {
     }
 }
 
+
+// 외계행성의 나이
+
+class Solution {
+    public String solution(int age) {
+        String answer = "";
+        String[] arr = String.valueOf(age).split("");
+        
+        for(int i=0; i<arr.length; i++){
+            answer+=((char)(Integer.parseInt(arr[i])+97));
+        }
+        return answer;
+    }
+}
