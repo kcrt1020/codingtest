58b08b8b63da4f4edd9736f8e995a5cd34b14f66
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-14 20:54:30 +0900

    20230814

--- programmers/20230812.java
+++ programmers/20230812.java
@@ -33,3 +33,24 @@ class Solution {
         return answer;
     }
 }
+
+
+// 진료 순서 정하기
+
+class Solution {
+    public int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+        for(int i=0; i<answer.length; i++){
+            answer[i]=1;
+        }
+        
+        for(int i=0; i<emergency.length; i++){
+            for(int j=0; j<emergency.length; j++){
+                if(emergency[i]<emergency[j]){
+                    answer[i]++;
+                }
+            }
+        }
+        return answer;
+    }
+}
