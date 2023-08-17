2c1665dcc93406ff72ece1a4b326d91a37ddb050
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-17 16:53:45 +0900

    20230817

--- programmers/20230817.java
+++ programmers/20230817.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 // 수열과 구간 쿼리 4
 
 class Solution {
@@ -10,5 +12,29 @@ class Solution {
             }
         }
         return arr;
+    }
+}
+
+
+// 배열 만들기 2
+
+class Solution {
+    public int[] solution(int l, int r) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=l; i<=r; i++){
+            String str=i+"";
+            int count=0;
+            for(int j=0; j<str.length(); j++){
+                if(str.charAt(j)==48 || str.charAt(j)==53){
+                    count++;
+                }
+            }
+            if(count == str.length()){
+                list.add(i);
+            }
+        }
+        
+        return list.isEmpty()? new int[] {-1} : list.stream().mapToInt(i->i).toArray();
     }
 }
