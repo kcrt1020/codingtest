073aa2b6290cfe8ea79a8a787030f239cc5131f9
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-17 22:49:26 +0900

    20230817

--- programmers/20230817.sql
+++ programmers/20230817.sql
@@ -12,3 +12,11 @@ SELECT animal_id, name from animal_ins order by animal_id;
 
 // 여러 기준으로 정렬하기
 SELECT animal_id, name, datetime from animal_ins order by name asc, datetime desc;
+
+// 상위 n개 레코드
+select name from (SELECT name from animal_ins order by datetime) where rownum=1;
+
+// 조건에 맞는 회원수 구하기
+SELECT count(*) as "user" from user_info 
+where joined >= to_date('2021-01-01','YYYY-MM-DD') and joined < to_date('2022-01-01','YYYY-MM-DD')
+and age>=20 and age<=29;
