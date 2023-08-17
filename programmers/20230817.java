dff66166352264e56b84cfe6c8f5e299437102c1
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-17 16:11:35 +0900

    20230817

---
+++ programmers/20230817.java
@@ -0,0 +1,14 @@
+// 수열과 구간 쿼리 4
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for(int i=0; i<queries.length; i++){
+            for(int j=queries[i][0]; j<=queries[i][1]; j++){
+                if(j%queries[i][2]==0){
+                    arr[j]++;
+                }
+            }
+        }
+        return arr;
+    }
+}
