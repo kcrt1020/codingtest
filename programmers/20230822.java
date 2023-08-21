6eba3c56031c1c9b619cf156238813043f6c195e
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-22 00:33:19 +0900

    20230822

---
+++ programmers/20230822.java
@@ -0,0 +1,16 @@
+// 2016년
+
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        String[] week = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        int[] month = {31,29,31,30,31,30,31,31,30,31,30,31};
+        int count = -1;
+        
+        for(int i=0; i<a-1; i++){
+            count+=month[i];
+        }
+        
+        return week[(count+b)%7];
+    }
+}
