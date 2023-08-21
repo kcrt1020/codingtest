7149658bdef828a696db58ee2c7d71d420d39612
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-21 20:19:34 +0900

    20230821

--- programmers/sql/20230821.sql
+++ programmers/sql/20230821.sql
@@ -19,3 +19,13 @@ SELECT MCDP_CD as "진료과 코드", count(*) as "5월예약건수" from APPOIN
 where to_char(APNT_YMD, 'YYYY-MM') = '2022-05'
 group by MCDP_CD
 order by count(*), MCDP_CD;
+
+// 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기
+SELECT CAR_TYPE, count(*) as CARS from CAR_RENTAL_COMPANY_CAR 
+where OPTIONS like '%통풍시트%'
+or OPTIONS like '%열선시트%'
+or OPTIONS like '%가죽시트%'
+group by CAR_TYPE
+order by CAR_TYPE;
+
+// 자동차 평균 대여 기간 구하기
