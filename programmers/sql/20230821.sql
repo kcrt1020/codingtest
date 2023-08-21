1903225d1bfc12236907ab96cc992d7f20148a46
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-21 20:28:21 +0900

    20230821

--- programmers/sql/20230821.sql
+++ programmers/sql/20230821.sql
@@ -29,3 +29,9 @@ group by CAR_TYPE
 order by CAR_TYPE;
 
 // 자동차 평균 대여 기간 구하기
+SELECT CAR_ID, round(avg((END_DATE)-(START_DATE)+1),1) as AVERAGE_DURATION 
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY 
+group by CAR_ID
+having round(avg((END_DATE)-(START_DATE)+1),1) >= 7
+order by round(avg((END_DATE)-(START_DATE)+1),1) desc, CAR_ID desc;
+
