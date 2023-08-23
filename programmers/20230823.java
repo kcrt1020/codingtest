01a8b47283c5054e8cc25fb4e6d1ecf169834fed
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-23 17:45:54 +0900

    20230823

--- programmers/20230823.java
+++ programmers/20230823.java
@@ -129,3 +129,25 @@ class Solution {
             return s.substring((s.length()/2), (s.length()/2)+1 );
     }
 }
+
+
+// 같은 숫자는 싫어
+
+public class Solution {
+    public int[] solution(int []arr) {
+        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(arr[0]);
+        for(int i=1; i<arr.length; i++){
+            if(list.get(list.size()-1)!=arr[i])
+            list.add(arr[i]);
+        }
+
+        int[] result = new int[list.size()];
+        
+        for(int i=0; i<list.size(); i++){
+            result[i]=list.get(i);
+        }
+        return result;
+    }
+}
