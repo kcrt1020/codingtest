f26f49dd16448423aeff1f466f7a7c7ba035f300
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-23 22:02:35 +0900

    20230823

--- programmers/20230823.java
+++ programmers/20230823.java
@@ -304,3 +304,44 @@ class Solution {
         return answer;
     }
 }
+
+
+// 소수 찾기
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        // for(int i=2; i<=n; i++){
+        //     boolean b = true;
+        //     for(int j=2; j<i; j++){
+        //         if(i%j==0){
+        //             b = false;
+        //             break;
+        //         }
+        //     }
+        //     if(b==true) answer++;
+        // }
+        
+        boolean[] prime = new boolean[n+1];
+        for(int i=2; i<=n; i++){
+            prime[i]=true;
+        }
+        
+        int root=(int)Math.sqrt(n);
+        
+        for(int i=2; i<=root; i++){
+            if(prime[i]==true){
+                for(int j=i; i*j<=n; j++){
+                    prime[i*j]=false;
+                }
+            }
+        }
+        
+        for(int i=2; i<=n; i++){
+            if(prime[i]==true) answer++;
+        }
+        
+        
+        return answer;
+    }
+}
