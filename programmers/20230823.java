1a93d2c38a25c70a3b03b08b971fbd307f2a497f
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-23 17:35:24 +0900

    20230823

--- programmers/20230823.java
+++ programmers/20230823.java
@@ -117,3 +117,15 @@ class Solution {
         return builder.toString();
     }
 }
+
+
+// 가운데 글자 가져오기
+
+class Solution {
+    public String solution(String s) {
+        if(s.length()%2==0) 
+            return s.substring((s.length()/2)-1, (s.length()/2)+1);
+        else 
+            return s.substring((s.length()/2), (s.length()/2)+1 );
+    }
+}
