4445701b66d72030b33d69446ae0d29742b0f836
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-24 20:17:18 +0900

    20230824

--- programmers/20230824.java
+++ programmers/20230824.java
@@ -289,3 +289,22 @@ class Solution {
         return answer;
     }
 }
+
+
+// 삼총사
+
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        
+        for(int i=0; i<number.length; i++){
+            if(i+2>=number.length) break;
+            for(int j=i+1; j<number.length; j++){
+                for(int k=j+1; k<number.length; k++){
+                    if(number[i]+number[j]+number[k]==0) answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
