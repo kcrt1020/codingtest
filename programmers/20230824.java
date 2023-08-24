e73056f03e8b250e503f0156b43616837034f6af
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-24 20:05:57 +0900

    20230824

--- programmers/20230824.java
+++ programmers/20230824.java
@@ -272,3 +272,20 @@ class Solution {
         return answer;
     }
 }
+
+
+// 예산
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for(int i=0; i<d.length; i++){
+            if(budget-d[i]<0) break;
+            
+            budget-=d[i];
+            answer++;
+        }
+        return answer;
+    }
+}
