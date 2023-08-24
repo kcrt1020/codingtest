7e1b19e70452a84992cce1ea7cf1b3de52f52666
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-24 16:57:32 +0900

    20230824

--- programmers/20230824.java
+++ programmers/20230824.java
@@ -229,3 +229,18 @@ class Solution {
         return answer;
     }
 }
+
+
+// 직사각형 별찍기
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        for(int i=0; i<b; i++){
+            System.out.println("*".repeat(a));
+        }
+    }
+}
