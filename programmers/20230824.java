05feb74e97d7f35947abc76d0173d2f56f09233d
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-24 22:18:57 +0900

    20230824

--- programmers/20230824.java
+++ programmers/20230824.java
@@ -405,3 +405,25 @@ class Solution {
         return answer;
     }
 }
+
+
+// K번째수
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i=0; i<commands.length; i++){
+            int[] arr = new int[commands[i][1]-commands[i][0] + 1 ];
+            int count=0;
+            for(int j=commands[i][0]-1; j<commands[i][1]; j++){
+                arr[count]=array[j];
+                count++;
+            }
+            Arrays.sort(arr);
+            answer[i]=arr[commands[i][2]-1];
+        }
+        
+        return answer;
+    }
+}
