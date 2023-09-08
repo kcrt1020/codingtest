ff9d9c09e6d4df77ac046152060e9ef9c45de5ca
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-09-08 21:57:14 +0900

    20230908

---
+++ programmers/20230908.java
@@ -0,0 +1,15 @@
+// 크기가 작은 부분문자열
+
+class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+        int p_length = p.length();
+        for(int i=0; i<t.length()-p_length+1; i++){
+            for(int j=i; j<i+p_length; j++){
+                System.out.println(i+" "+j);
+                
+            }
+        }
+        return answer;
+    }
+}
