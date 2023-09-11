ad03eb1a9ea7a14d0fa7dbb98c852360a8032293
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-09-11 21:00:07 +0900

    20230911

--- programmers/20230911.java
+++ programmers/20230911.java
@@ -21,3 +21,17 @@ class Solution {
         return answer;
     }
 }
+
+
+// 음양 더하기
+
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i=0; i<absolutes.length; i++) {
+            
+            answer += signs[i]==true ? absolutes[i] : absolutes[i] *(-1);
+        }
+        return answer;
+    }
+}
