b63424e4ff07e9c5cab080d968b5d1e4390de23f
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-09-11 23:35:36 +0900

    20230911

--- programmers/20230911.java
+++ programmers/20230911.java
@@ -35,3 +35,18 @@ class Solution {
         return answer;
     }
 }
+
+
+// 부족한 금액 계산하기
+
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        
+        for(int i=1; i<=count; i++){
+            answer+=price*i;
+        }
+
+        return answer>money?answer-money:0;  
+    }
+}
