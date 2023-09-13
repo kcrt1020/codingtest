f4f3cd8ce6f46423245514e348a01ecec4c5cc4d
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-09-13 20:44:24 +0900

    20230913

--- programmers/20230913.java
+++ programmers/20230913.java
@@ -30,3 +30,41 @@ class Solution {
         return answer;
     }
 }
+
+
+// 최솟값 만들기
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for(int i=0; i<A.length; i++){
+            answer+=A[i]*B[B.length-1-i];
+        }
+
+        return answer;
+    }
+}
+// 1차 시간초과
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for(int i=0; i<A.length; i++){
+            answer=answer+(A[i]*B[B.length-1-i]);
+        }
+
+        return answer;
+    }
+}
