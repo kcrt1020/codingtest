17b9fd58a8934a09a2eba62b42eb18e791bea960
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-09-14 01:39:30 +0900

    20230914

--- programmers/20230914.java
+++ programmers/20230914.java
@@ -22,12 +22,30 @@ class Solution {
 class Solution {
     public int solution(int n) {
         int answer = n+1;
+        String num=(Integer.toBinaryString(n)).replace("0","");
         
         while(true){
-            if((Integer.toBinaryString(n)).replace("0","").length()==(Integer.toBinaryString(answer)).replace("0","").length()) break;
+            if(num.length()==(Integer.toBinaryString(answer)).replace("0","").length()) break;
             answer++;
         }
         
         return answer;
     }
+}
+
+
+// 피보나치 수
+
+class Solution {
+    public static int solution(int n) {
+        int[] D = new int[n + 1];
+        D[0] = 0;
+        D[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            D[i] = (D[i - 1] + D[i - 2]) % 1234567;
+        }
+
+        return D[n];
+    }
+
 }
