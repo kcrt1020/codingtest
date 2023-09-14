d3453215311cc38792a16d022951e76cade0c0db
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-09-15 03:24:02 +0900

    20230915

--- programmers/20230915.java
+++ programmers/20230915.java
@@ -70,3 +70,13 @@ class Solution {
         return answer;
     }
 }
+
+
+// 공백으로 구분하기 1
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = my_string.split(" ");
+        return answer;
+    }
+}
