55089928023fab2d21b9b3be5259e485b43ab165
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-09-15 03:25:27 +0900

    20230915

--- programmers/20230915.java
+++ programmers/20230915.java
@@ -80,3 +80,12 @@ class Solution {
         return answer;
     }
 }
+
+
+// 소문자로 바꾸기
+
+class Solution {
+    public String solution(String myString) {
+        return myString.toLowerCase();
+    }
+}
