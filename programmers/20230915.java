d1b39095cf34aa63c8e4fda8f779fe7a626b2f17
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-09-15 03:01:58 +0900

    20230915

---
+++ programmers/20230915.java
@@ -0,0 +1,29 @@
+// 세로 읽기
+
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        String answer = "";
+        while(true){
+            answer+=my_string.charAt(c-1);
+            c=m+c;
+            
+            if(c>my_string.length()) break;
+        }
+        return answer;
+    }
+}
+
+// 날짜 비교하기
+
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        int answer = 0;
+        
+        if(date1[0]>date2[0]) return 0;
+        else if(date1[0]<date2[0]) return 1;
+        else if(date1[1]<date2[1]) return 1;
+        else if(date1[1]>date2[1]) return 0;
+        else if(date1[2]<date2[2]) return 1;
+        else return 0;
+    }
+}
