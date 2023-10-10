82e72e9bb2226d8b5df304e6f2a197e9b0b73c93
kcrt1020 <kcrt1020@naver.com>
2023-10-10 23:09:58 +0900

    20231010

--- programmers/20230915.java
+++ programmers/20230915.java
@@ -24,7 +24,7 @@ class Solution {
         else if(date1[1]<date2[1]) return 1;
         else if(date1[1]>date2[1]) return 0;
         else if(date1[2]<date2[2]) return 1;
-        else return 0; 
+        else return 0;  
     }
 }
 
