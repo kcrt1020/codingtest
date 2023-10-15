5c71ecad5a6397213769c0985ff739d305a396f7
kcrt1020 <kcrt1020@naver.com>
2023-10-15 22:24:02 +0900

    20231015

--- programmers/20230915.java
+++ programmers/20230915.java
@@ -17,7 +17,7 @@ class Solution {
 
 class Solution {
     public int solution(int[] date1, int[] date2) {
-        int answer = 0;
+        int answer = 0; 
         
         if(date1[0]>date2[0]) return 0;
         else if(date1[0]<date2[0]) return 1;
