d8162626e66236e5e593a9b2afb3bff3bdc981ef
kcrt1020 <kcrt1020@naver.com>
2023-10-20 23:57:08 +0900

    20231020

--- programmers/20231020.java
+++ programmers/20231020.java
@@ -1,1 +1,42 @@
 // 점프와 순간 이동
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        
+//         for(int i=1; i<=n; i++){
+//             int temp=i;
+//             int loc=n;
+            
+//             while(loc>0){
+//                 if(temp*2<loc){
+//                     temp*=2;
+//                     System.out.println(temp);
+//                 }
+//                 else {
+//                     if(i==1){
+//                         ans=loc-temp+i;
+//                         System.out.println(loc-temp);
+//                     }
+//                     else{
+//                         if(ans>loc-temp+i && 0<loc-temp+i) ans=loc-temp+i;
+//                         System.out.println(loc-temp+i);
+//                     }
+//                     break;
+//                 }
+//             }
+//             System.out.println("===");
+//         }
+        
+        while(n != 0) {
+            if(n%2==0){
+                n/=2;
+            }else{
+                n--;
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
