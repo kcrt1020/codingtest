ec682aa1363fd7ff7eb1d818fb5f9f68f6a61c58
kcrt1020 <kcrt1020@naver.com>
2023-10-24 02:07:27 +0900

    20231024

--- programmers/20231023.java
+++ programmers/20231023.java
@@ -18,3 +18,34 @@ class Solution {
         return d[n];
     }
 }   // 피보나치
+
+
+// 귤 고르기
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int t : tangerine){
+            map.put(t, map.getOrDefault(t,0)+1);
+        }
+        
+        for(Integer key:map.keySet()){
+            pq.add(map.get(key));
+        }
+        
+        int count=0;
+        while(k>0){
+            if(pq.peek()>k){
+                pq.poll();
+                count++;
+                break;
+            }else{
+                k-=pq.poll();
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
