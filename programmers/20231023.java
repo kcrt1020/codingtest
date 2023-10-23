59fa6f45015baafe73cef25e442e6e09c82cd208
kcrt1020 <kcrt1020@naver.com>
2023-10-24 03:59:46 +0900

    20231024

--- programmers/20231023.java
+++ programmers/20231023.java
@@ -49,3 +49,27 @@ class Solution {
         return count;
     }
 }
+
+
+// 연속 부분 수열 합의 개수
+
+class Solution {
+    public int solution(int[] elements) {
+        Set<Integer> ans = new HashSet<>();
+        
+        for(int i=1; i<=elements.length; i++){
+            for(int j=0; j<elements.length; j++){
+                int sum=0;
+                for(int k=0; k<i; k++) {
+                    if(j+k>elements.length-1){
+                        sum+=elements[j+k-elements.length];
+                    }else{
+                        sum+=elements[j+k];
+                    }
+                }
+                ans.add(sum);
+            }
+        }
+        return ans.size();
+    }
+}
