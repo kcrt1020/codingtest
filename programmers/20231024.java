1bbb4ec369ac906a111f855e0b355719e9bafb02
kcrt1020 <kcrt1020@naver.com>
2023-10-25 04:22:27 +0900

    20231024

--- programmers/20231024.java
+++ programmers/20231024.java
@@ -57,3 +57,15 @@ class Solution {
         return (int)flo;
     }
 }
+
+
+// 첫 번째로 나오는 음수
+
+class Solution {
+    public int solution(int[] num_list) {
+        for(int i=0; i<num_list.length; i++){
+            if(num_list[i]<0) return i;
+        }
+        return -1;
+    }
+}
